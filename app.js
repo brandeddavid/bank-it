fe94a523d7e39c73fbf5775b8d96d4a5d7d65599
David Mwangi <david.mathenge98@gmail.com>
2019-05-03 16:59:36 +0300

    chore(setup): restructure project folder and run application

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 dotenv.config()
 const app = express();
-const port = process.env.PORT || 5000
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).send({ status: 200, message: "Welcome to Bank It App" })
 })
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`)
-});
+module.exports = app;
---
+++ server/models/user/index.js
@@ -0,0 +1,35 @@
+const sequelize = require('sequelize');
+
+/**
+ * 
+ * @param { object } sequelize 
+ * @param { object } DataTypes 
+ */
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  })
+}
+
+export default user;
